94e4c56acc2c2a91dd93c5204ed2de1df63e20c1
Ouarkoub Camelea <ouarkoubc@b11p4.fil.univ-lille1.fr>
2019-11-04 12:07:28 +0100

    partie 6 du tp2

--- src/main/java/DispatcherServlet.java
+++ src/main/java/DispatcherServlet.java
@@ -4,6 +4,8 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Map;
@@ -14,11 +16,43 @@ public class DispatcherServlet extends HttpServlet {
     private Map<String, Method> uriMappings = new HashMap<>();
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         System.out.println("Getting request for " + req.getRequestURI());
-        // TODO 
-    }
+        var uri = req.getRequestURI();
 
+        if(! uriMappings.containsKey(uri)){
+            resp.sendError(404, "no mapping found for request uri " + uri);
+            return;
+        }
+
+        var method = getMappingForUri(uri);
+        try {
+            // getting new instancea
+            var instance = method.getDeclaringClass().newInstance();
+
+            // getting params
+            var params = req.getParameterMap();
+
+            // calling method with params if needed
+            Object result;
+            if(method.getParameterCount() > 0 ){
+                result = method.invoke(instance, params);
+            }
+            else {
+                result = method.invoke(instance);
+            }
+
+            // sending response
+            resp.getWriter().print(result.toString());
+        } catch (InstantiationException | IllegalAccessException e) {
+            // default exception handling
+            e.printStackTrace();
+        }
+        catch( InvocationTargetException e){
+            // when getting an exception, sending it to the client
+            resp.sendError(500, "exception when calling method " + method.getName() + " : " + e.getCause().getMessage());
+        }
+    }
     @Override
     public void init(ServletConfig config) throws ServletException {
         super.init(config);
@@ -28,11 +62,9 @@ public class DispatcherServlet extends HttpServlet {
 
     protected void registerController(Class controllerClass){
         if (controllerClass.isAnnotationPresent(Controller.class)) {
-            Method[] methods = controllerClass.getMethods();
-            if (methods.length != 0) {
-                for (int i = 0; i < methods.length; i++) {
-                    registerMethod(methods[i]);
-                }
+
+            for (Method method : controllerClass.getDeclaredMethods()) {
+                this.registerMethod(method);
             }
         }
         else{
---
+++ src/main/java/com/ifi/controller/PokemonTypeController.java
@@ -0,0 +1,14 @@
+package com.ifi.controller;
+
+import com.ifi.bo.PokemonType;
+import com.ifi.repository.PokemonTypeRepository;
+
+import java.util.Map;
+
+public class PokemonTypeController {
+    private PokemonTypeRepository repository = new PokemonTypeRepository();
+
+    public PokemonType getPokemon(Map<String,String[]> parameters){
+
+    }
+}
